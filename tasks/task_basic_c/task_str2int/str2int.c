2018292456be98b857afac85a4488f14b7e8fe8b
Eugene <super.molchanov@list.ru>
2024-10-07 21:36:20 +0300

    Homework 1

--- tasks/task_basic_c/task_str2int/str2int.c
+++ tasks/task_basic_c/task_str2int/str2int.c
@@ -1,10 +1,49 @@
 #include <assert.h>
-#include "str2int.h"
+#include <stdio.h>
 
+const int max = 2147483647;
+const int min = -2147483648;
+
+int is_space(char c) {
+    return c == ' ';
+}
+
+int is_digit(char c) {
+    return c >= '0' && c <= '9';
+}
 
 int str2int(const char *str) {
-    if (*str == '-') {
-        assert(1 == 0);
+    int sign = 1;
+    int result = 0;
+
+    assert(str != NULL && *str != '\0');
+
+    while (is_space(*str)) {
+        str++;
     }
-    return 0;
+
+    if (*str == '-') {
+        sign = -1;
+        str++;
+    } else if (*str == '+') {
+        str++;
+    }
+
+    assert(*str != '\0');
+
+    while (is_digit(*str)) {
+        int digit = *str - '0';
+
+        if (sign == 1 && (result > (max - digit) / 10)) {
+            assert(0);
+        }
+        if (sign == -1 && (result > (min - digit) / 10)) {
+            assert(0);
+        }
+
+        result = result * 10 + digit;
+        str++;
+    }
+
+    return sign * result;
 }
