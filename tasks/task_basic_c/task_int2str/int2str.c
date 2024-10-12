27e18d7b46c9b21facab23e16860c46273c9e6c7
Eugene <super.molchanov@list.ru>
2024-10-12 10:10:52 +0300

    Homework 2

--- tasks/task_basic_c/task_int2str/int2str.c
+++ tasks/task_basic_c/task_int2str/int2str.c
@@ -1,8 +1,43 @@
 #include <assert.h>
-#include "int2str.h"
 #include "stdio.h"
+#include <limits.h>
 
-char* int2str(int number) {
-    char* str = "0";
-    return str;
+char* int2str(int num) {
+    static char str[12];
+    int i = 10;
+    int isNegative = 0;
+
+    str[11] = '\0';
+
+    if (num == INT_MIN) {
+        return "-2147483648";
+    }
+    if (num == INT_MAX) {
+        return "2147483647";
+    }
+
+    if (num == 0) {
+        str[i] = '0';
+        return &str[i];
+    }
+
+    if (num < 0) {
+        isNegative = 1;
+        num = -num;
+    }
+
+    while (num > 0 && i >= 0) {
+        str[i--] = (num % 10) + '0';
+        num /= 10;
+    }
+
+    if (isNegative && i >= 0) {
+        str[i--] = '-';
+    }
+
+    return &str[i + 1];
+}
+int main(void){
+    printf("%s\n", int2str(2147483649));
+    return 0;
 }
